f97c9f509b68466b22a91de467d1ef0767976f51
bekirytm <bekir.yetim.06@gmail.com>
2020-04-10 21:22:27 +0300

    find and update endpoint edits

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+
 const indexRouter = require('./routes/index');
 const movieRouter = require('./routes/movie');
 
@@ -23,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/movie', movieRouter);
+app.use('/api/movies', movieRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -38,7 +39,8 @@ app.use((err, req, res, next) =>  {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error : { message : err.message  ,  code: err.code }});
+
 });
 
 module.exports = app;
--- routes/movie.js
+++ routes/movie.js
@@ -9,9 +9,66 @@ const router = express.Router();
 const Movie = require('../models/Movie');
 
 
+// ****** BÜTÜN FİLMLERİ GETİRME *******
+// AÇIKLAMA : Burada db'ye kayıtlı tüm filmleri getiren endpoint'i yaptık.
+
+router.get('/' , (req,res) => {
+  const promise = Movie.find({});
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) =>{
+    res.json(err);
+  });
+
+});
+
+
+// ****** ID' ye GÖRE ARAMA YAPMA ****** (movie_id bazlı arama)
+// AÇIKLAMA : Burada film id'lerine göre arama yaptık ve o id'ye sahip filmi getirdik.
+
+router.get('/:movie_id' , (req,res,next) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((data) => {
+    // Burada hata yönetimi yaptık (o id'de movie yoksa)
+    if(!data){
+      next({ message : " The movie was not found ." , code : 109 });
+    }else {
+      res.json(data);
+    }
+
+  }).catch((err) => {
+    res.json(err);
+  })
+
+});
+
+
+// ****** GÜNCELLEME İŞLEMİ ******
+// Burada istenilen filme ait değişiklikler , güncellemeler yaptık.
+router.put('/:movie_id' , (req,res,next) => {
+  const promise = Movie.findByIdAndUpdate( req.params.movie_id , req.body );   // hangi kayıt , hangi değişiklikler
+
+  promise.then((data) => {
+    if(!data){
+      next({ message:  " The movie was not found ." , code : 108 });
+    }else{
+      res.json(data);
+    }
+  }).catch((err) => {
+    res.json(err);
+  })
+
+});
+
+
+
+
+
+// ****** YENİ FİLM OLUŞTURMA ******
+
 router.post('/', (req, res, next) => {
-
-
 
   // YENİ YÖNTEM (Burada karşıdan gelen verileri req ile aldık ve promise ile kaydettik.)
 
