7dab3f577448c8559a939e8d57b3259f23eadb62
bekirytm <bekir.yetim.06@gmail.com>
2020-04-14 17:47:34 +0300

    api/directors/:director_id [GET]

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -20,6 +21,7 @@ router.post('/', (req, res) =>  {
 });
 
 
+// Bütün Yönetmenler ve Filmlerini Getirme :
 router.get('/' , (req,res) => {
   const promise = Director.aggregate([
     {
@@ -74,6 +76,65 @@ router.get('/' , (req,res) => {
 });
 
 
+// Director_id Bazlı Yönetmen Arama , Getirme :
+router.get('/:director_id' , (req,res) => {
+  const promise = Director.aggregate([
+    {
+      // Koşullar burada konulur.
+      $match : {
+        '_id' : mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      // Burada Join işlemi yapılır.(Bağlama)
+      $lookup: {
+        from : 'movies',
+        localField : '_id',
+        foreignField : 'director_id',
+        as : 'movies'
+      }
+    },
+    {
+      // Yukarıdaki bağlantıyı kullanabilmek için .
+      $unwind : {
+        path : '$movies',
+        preserveNullAndEmptyArrays : true    // Bunun anlamı yönetmenin filmi olmasa da göster.
+      }
+    },
+    {
+      //Bir Yönetmene ait tüm filmleri beraber göstermesi için.
+      $group:{
+        _id: {
+          _id : '$_id',
+          name : '$name',
+          surname : '$surname',
+          bio : '$bio'
+        },
+        movies : {
+          $push : '$movies'
+        }
+      }
+    },
+    {
+      // Gösterme işleminde id yazmasın diye.
+      $project : {
+        _id : '$_id._id',
+        name : '$_id.name',
+        surname : '$_id.surname',
+        movies : '$movies'
+      }
+    }
+
+
+  ]);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+
+});
 
 
 module.exports = router;
