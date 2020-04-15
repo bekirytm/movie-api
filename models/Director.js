28d27676b19f067ae09a19e8710fdc6baa95a560
bekirytm <bekir.yetim.06@gmail.com>
2020-04-15 19:49:56 +0300

    api/directors [POST] validation edit

--- models/Director.js
+++ models/Director.js
@@ -4,9 +4,21 @@ const Schema = mongoose.Schema;
 // Director Şeması :
 
 const DirectorSchema = new Schema({
-    name : String,
-    surname : String,
-    bio : String,
+    name : {
+        type: String,
+        maxlength : 60,
+        minlength : 2
+    },
+    surname : {
+        type: String,
+        maxlength: 60,
+        minlength: 2
+    },
+    bio : {
+        type: String,
+        maxlength : 1000,
+        minlength : 50
+    },
     createAt : {
         type: Date,
         default : Date.now
