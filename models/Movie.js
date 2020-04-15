b3094504d81707336bc468931bfc67987d978634
bekirytm <bekir.yetim.06@gmail.com>
2020-04-15 19:43:01 +0300

    api/movies Schema validation edit

--- models/Movie.js
+++ models/Movie.js
@@ -8,12 +8,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true , '{PATH} alanı zorunludur.'],
+        maxlength : [20 , '`{PATH}` alanı (`{VALUE}`) , ({MAXLENGTH}) karakterden büyük olamaz.'],
+        minlength : [3 , '`{PATH}` alanı (`{VALUE}`) , ({MINLENGTH})  karakterden küçük olamaz.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    country: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    year: {
+        type: Number,
+        max: 2050,
+        min: 1900
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
