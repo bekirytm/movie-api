1c93031b593c4df5ac0410d0892f39841904c016
bekirytm <bekir.yetim.06@gmail.com>
2020-04-17 17:55:04 +0300

    bcrypt password hash

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+//bcrypt
+const bcrypt = require('bcryptjs');
+
 //Models
 const User = require('../models/User');
 
 router.post('/register' , (req,res,next) => {
   const { username , password } = req.body;
-  const user = new User({
-    username : username,
-    password : password
-  });
 
-  const promise = user.save();
-  promise.then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.json(err);
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      username : username,
+      password : hash
+    });
+
+    const promise = user.save();
+    promise.then((data) => {
+      res.json(data);
+
+    }).catch((err) => {
+      res.json(err);
+    });
   });
+});
+
+
+router.get('/pass' , (req,res) => {
 
 });
 
