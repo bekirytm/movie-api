6a110472796f3d9de1c364c9af953df9caf29366
bekirytm <bekir.yetim.06@gmail.com>
2020-04-17 17:10:28 +0300

    /register [POST]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) =>  {
-  res.render('index', { title: 'Express' });
+//Models
+const User = require('../models/User');
+
+router.post('/register' , (req,res,next) => {
+  const { username , password } = req.body;
+  const user = new User({
+    username : username,
+    password : password
+  });
+
+  const promise = user.save();
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+
 });
 
 module.exports = router;
