61cfb73e46493a1047e9bacee27990828760d6d4
bekirytm <bekir.yetim.06@gmail.com>
2020-04-19 03:23:12 +0300

    jwt token edits

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 //bcrypt
 const bcrypt = require('bcryptjs');
 
+//jwt
+const jwt = require('jsonwebtoken');
+
 //Models
 const User = require('../models/User');
 
+
+// KAYIT OLMA
 router.post('/register' , (req,res,next) => {
   const { username , password } = req.body;
 
@@ -38,20 +43,36 @@ router.post('/authenticate' , (req,res) => {
     }
 
     if(!user){
+      // Kullanıcı adı kayıtlı değilse:
       res.json({
         status : false ,
         message : 'Autentication failed , user not found. '
       });
     }
     else{
+      // Kullanıcı adı var ise şifre karşılaştırma:
       bcrypt.compare(password , user.password).then((result) => {
         if(!result){
+          // Şifre karşılaştırma başarısızsa:
           res.json({
             status : false  ,
             message : 'Authentication failed , wrong password'
-          })
+          });
         }
         else{
+          // Şifre Krşılaştırma başarılı ise Token Oluşturma:
+          const payload = {
+            username
+          };
+
+          const token = jwt.sign(payload , req.app.get('api_secret_key') , {
+            expiresIn : 720 // 12 saat
+          });
+
+          res.json({
+            status : true ,
+            token
+          });
 
         }
       });
